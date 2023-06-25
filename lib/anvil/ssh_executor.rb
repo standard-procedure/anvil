93d58984585b49b7d2288f55583e055ff381b96e
Rahoul Baruah <rahoulb@echodek.co>
2023-06-25 15:57:23 +0000

    Removed use_sudo from SSHExecutor
    No longer needed as we don't have installation connections - that is now handled by cloudinit

--- lib/anvil/ssh_executor.rb
+++ lib/anvil/ssh_executor.rb
@@ -8,32 +8,18 @@ require "net/ssh"
 # - with sudo it creates a script on the remote server, runs it via sudo, and then deletes it
 # If supplied, it will also write the output of the script to a logger.
 module Anvil
-  class SshExecutor < Struct.new(:hostname, :user, :use_sudo, :logger)
+  class SshExecutor < Struct.new(:hostname, :user, :logger)
     def call &block
       @connection = Net::SSH.start hostname, user, use_agent: true
       block.call self
     end
 
     def exec! script, category = ""
-      method = use_sudo ? :exec_with_sudo : :exec_without_sudo
-      send(method, script, category) do |channel, stream, data|
+      @connection.exec! script do |channel, stream, data|
         data.to_s.split("\n") do |line|
           logger&.info line, category
         end
       end
-    end
-
-    protected
-
-    def exec_without_sudo script, category = "", &block
-      @connection.exec! script, &block
-    end
-
-    def exec_with_sudo script, category = "", &block
-      @connection.exec! "cat >> exec.sh << SCRIPT\n#{script}\nSCRIPT", &block
-      @connection.exec! "chmod 755 exec.sh", &block
-      @connection.exec! "sudo ./exec.sh", &block
-      @connection.exec! "rm exec.sh", &block
     end
   end
 end
--- spec/anvil/ssh_executor_spec.rb
+++ spec/anvil/ssh_executor_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative "../../lib/anvil/ssh_executor"
 RSpec.describe Anvil::SshExecutor do
+  subject { Anvil::SshExecutor.new("server1.example.com", "user", logger) }
   let(:logger) { double "logger", info: true }
   let(:ssh_connection) { double "net/ssh" }
 
@@ -9,48 +10,19 @@ RSpec.describe Anvil::SshExecutor do
     expect(Net::SSH).to receive(:start).with("server1.example.com", "user", use_agent: true).and_return(ssh_connection)
   end
 
-  context "without sudo" do
-    subject { Anvil::SshExecutor.new("server1.example.com", "user", false, logger) }
-
-    it "creates an SSH connection and yields to the caller" do
-      subject.call do |executor|
-        expect(executor).to be_a(Anvil::SshExecutor)
-      end
-    end
-
-    it "executes the given script and writes multiple lines to the logger" do
-      expect(ssh_connection).to receive(:exec!).with("script").and_yield(nil, nil, "line1\nline2\n")
-      expect(logger).to receive(:info).with("line1", "category")
-      expect(logger).to receive(:info).with("line2", "category")
-
-      subject.call do |executor|
-        executor.exec! "script", "category"
-      end
+  it "creates an SSH connection and yields to the caller" do
+    subject.call do |executor|
+      expect(executor).to be_a(Anvil::SshExecutor)
     end
   end
 
-  context "with sudo" do
-    subject { Anvil::SshExecutor.new("server1.example.com", "user", true, logger) }
+  it "executes the given script and writes multiple lines to the logger" do
+    expect(ssh_connection).to receive(:exec!).with("script").and_yield(nil, nil, "line1\nline2\n")
+    expect(logger).to receive(:info).with("line1", "category")
+    expect(logger).to receive(:info).with("line2", "category")
 
-    it "creates an SSH connection and yields to the caller" do
-      subject.call do |executor|
-        expect(executor).to be_a(Anvil::SshExecutor)
-      end
-    end
-
-    it "creates a script, runs it via sudo, and writes multiple lines to the logger" do
-      expect(ssh_connection).to receive(:exec!).with("cat >> exec.sh << SCRIPT\nscript\nSCRIPT").and_yield(nil, nil, "line1\n")
-      expect(ssh_connection).to receive(:exec!).with("chmod 755 exec.sh").and_yield(nil, nil, "line2\n")
-      expect(ssh_connection).to receive(:exec!).with("sudo ./exec.sh").and_yield(nil, nil, "line3\n")
-      expect(ssh_connection).to receive(:exec!).with("rm exec.sh").and_yield(nil, nil, "line4\n")
-      expect(logger).to receive(:info).with("line1", "category")
-      expect(logger).to receive(:info).with("line2", "category")
-      expect(logger).to receive(:info).with("line3", "category")
-      expect(logger).to receive(:info).with("line4", "category")
-
-      subject.call do |executor|
-        executor.exec! "script", "category"
-      end
+    subject.call do |executor|
+      executor.exec! "script", "category"
     end
   end
 end
